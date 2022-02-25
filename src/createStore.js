91b27b12c147fbd9cb8d0457d7d62da8749ef7e9
TaraMKelly <tara.kelly16@gmail.com>
2022-02-25 16:40:17 -0500

    redux practice

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,20 @@
-let state;
+function createStore(reducer) {
+  let state;
+  //dispatches actions that modify state
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+  //returns state to use elsewhere in our application
+  function getState() {
+    return state
+  }
+  //this is the store and contains all of the application's state
+  return {
+    dispatch,
+    getState
+  }
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +26,16 @@ function reducer(state = { count: 0 }, action) {
   }
 }
 
-function dispatch(action) {
-  state = reducer(state, action);
-  render();
-}
-
 function render() {
   let container = document.getElementById("container");
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 }
 
-dispatch({ type: "@@INIT" });
+let store = createStore(reducer)
+store.dispatch({ type: "@@INIT" });
+
 let button = document.getElementById("button");
 
 button.addEventListener("click", function () {
-  dispatch({ type: "counter/increment" });
+  store.dispatch({ type: "counter/increment" });
 });
